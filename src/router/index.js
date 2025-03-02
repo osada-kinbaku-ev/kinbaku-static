f725ee83435398580e3aeaf721e703609063e3ae
nropeb <160221632+nropeb@users.noreply.github.com>
2025-03-02 18:17:55 +0100

    Use hash-based history mode

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHashHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
 
@@ -18,7 +18,7 @@ app_routes.push(...[
 ])
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: app_routes,
 })
 
