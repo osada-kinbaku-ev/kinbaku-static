28cf696eade520848e5f4df5403d61e1603e1d95
nropeb <160221632+nropeb@users.noreply.github.com>
2025-03-15 19:21:27 +0100

    Adds app footer

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,9 +3,45 @@
     <router-view />
   </v-main>
 
-  <AppFooter />
+  <v-footer
+    class="d-flex flex-column align-stretch pa-0 white--text text--darken-1 elevation-12"
+  >
+    <div class="grey darken-3 d-sm-flex flex-row justify-space-between pa-4">
+      <div class="pa-2">
+        <b>Osada Kinbaku Dojo e.V.</b>
+      </div>
+      <div class="d-sm-flex flex-row align-right py-2">
+        <div class="px-2"><router-link to="datenschutz">Datenschutzerklärung</router-link></div>
+        <div class="px-2"><router-link to="impressum">Impressum</router-link></div>
+      </div>
+    </div>
+    <div class="grey darken-4 d-md-flex flex-row justify-space-between pa-6">
+      <div>
+        <p>{{ email }}</p>
+        <p>
+          Binzstraße 64<br/>
+          13189 Berlin-Pankow
+        </p>
+      </div>
+      <div>
+        <p>Registernummer</p>
+        <p>VR 29488 B at AG Berlin (Charlottenburg)</p>
+      </div>
+      <div>
+        <p>Vorstand</p>
+        <p class="white--text text--darken-2">
+          Alexander Esser<br/>
+          Anser Janczyk<br/>
+        </p>
+      </div>
+    </div>
+  </v-footer>
 </template>
 
-<script setup>
-  //
+<script>
+export default {
+  data: () => ({
+    email: atob("==QZk5ibpxmclJWL1tWYi5WarB0bm5Wa".split("").reverse().join("") )
+  }),
+}
 </script>
